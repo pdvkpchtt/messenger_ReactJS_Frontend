0a369c64532c0f733ed914a1631b11eb4cebcf69
Danil <kabirdanilov14@gmail.com>
2023-08-17 01:56:28 +0500

    added backend requests

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -46,12 +46,63 @@ const Auth = () => {
             </motion.div>
           ) : null}
 
-          <CustomButton text="Create account" />
+          <CustomButton
+            text="Create account"
+            onPress={() => {
+              const vals = { username: loginState, password: passwordState };
+
+              setPasswordState("");
+              setLoginState("");
+
+              fetch("http://localhost:4000/auth/register", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(vals),
+              })
+                .catch((err) => {
+                  return;
+                })
+                .then((res) => {
+                  if (!res || !res.ok || res.status >= 400) return;
+                  return res.json();
+                })
+                .then((data) => {
+                  if (!data) return;
+                  console.log(data);
+                });
+            }}
+          />
           <TextHead text="Already have an account?" />
           <CustomButton
             text="LogIn"
             onPress={() => {
-              setInvalid(!invalid);
+              const vals = { username: loginState, password: passwordState };
+
+              setPasswordState("");
+              setLoginState("");
+
+              fetch("http://localhost:4000/auth/login", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(vals),
+              })
+                .catch((err) => {
+                  return;
+                })
+                .then((res) => {
+                  if (!res || !res.ok || res.status >= 400) return;
+                  return res.json();
+                })
+                .then((data) => {
+                  if (!data) return;
+                  console.log(data);
+                });
             }}
           />
         </motion.div>
